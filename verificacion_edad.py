cdbd83d4b26beccbc86738e6fc7189999b53776c
Joshu <aren.daven1907@gmail.com>
2026-07-21 19:56:16 -0600

    Programa: Verificación de Edad

---
+++ verificacion_edad.py
@@ -0,0 +1,5 @@
+edad = int(input("Ingrese su edad: "))
+if edad >= 18:
+    print("Usted es mayor de edad, puede votar.Cuenta con: ", edad, "años")
+else:
+    print("Usted es menor de edad, no puede votar. Cuenta con: ", edad, "años")
